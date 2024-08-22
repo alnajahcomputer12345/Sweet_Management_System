8cefac0343d4fd82bfb80ed1bac06a3011e4eeb0
Ruba Abu Samra <rubaabusam@gmail.com>
2024-08-22 22:49:11 +0300

    Update Log_out_Steps.java

--- src/test/java/mysweetmanagementsystemtestcases/Log_out_Steps.java
+++ src/test/java/mysweetmanagementsystemtestcases/Log_out_Steps.java
@@ -7,6 +7,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import myAPP2024.myappsweet;
+import java.util.Scanner;
 
 public class Log_out_Steps {
 
