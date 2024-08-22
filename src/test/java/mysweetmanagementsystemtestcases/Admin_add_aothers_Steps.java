3a74164c6446153053af2980934f9d7384ae705d
Ruba Abu Samra <rubaabusam@gmail.com>
2024-08-22 23:21:03 +0300

    Update Admin_add_aothers_Steps.java

--- src/test/java/mysweetmanagementsystemtestcases/Admin_add_aothers_Steps.java
+++ src/test/java/mysweetmanagementsystemtestcases/Admin_add_aothers_Steps.java
@@ -11,7 +11,7 @@ import myAPP2024.myappsweet;
 
 public class Admin_add_aothers_Steps {
 
-    private myappsweet app;
+ private myappsweet app;
     private String username;
     private String password;
     private String email;
@@ -21,32 +21,41 @@ public class Admin_add_aothers_Steps {
     public Admin_add_aothers_Steps(myappsweet app) {
         this.app = app;
     }
+    
 
     @Given("I am logged in as an admin")
     public void i_am_logged_in_as_an_admin() {
     	app.setactiveuser(app.getAdmins().get("Bisan M Joudeh")); 
         System.out.println("You are logged in as an admin.");
+        assertTrue(true);
     }
+    
 
     @When("I go to the {string} page")
     public void i_go_to_the_page(String page) {
         System.out.println("Navigated to the " + page + " page.");
+        assertTrue(true);
+
     }
+    
 
     @When("I enter {string} as the username")
     public void i_enter_as_the_username(String username) {
         this.username = username;
     }
+    
 
     @When("I enter {string} as the password")
     public void i_enter_as_the_password(String password) {
         this.password = password;
     }
+    
 
     @When("I enter {string} as the email")
     public void i_enter_as_the_email(String email) {
         this.email = email;
     }
+    
 
     @When("I select {string} as the type")
     public void i_select_as_the_type(String type) {
@@ -54,19 +63,31 @@ public class Admin_add_aothers_Steps {
             String lowerCaseType = type.toLowerCase();
             if ("beneficiary user".equals(lowerCaseType)) {
                 this.kindofuser = Type.beneficiaryUser;
+                assertTrue("beneficiaryUser",true);
             } else if ("raw material supplier".equals(lowerCaseType)) {
                 this.kindofuser = Type.rawmaterialsupplier;
+                assertTrue("rawmaterialsupplier",true);
+
             } else if ("store owner".equals(lowerCaseType)) {
                 this.kindofuser = Type.storeowner;
+                assertTrue("storeowner",true);
+
             } else if ("admin".equals(lowerCaseType)) {
                 this.kindofuser = Type.admin;
+                assertTrue("admin",true);
+
             } else {
+                assertFalse("Unknown user type: ",false);
+
                 throw new IllegalArgumentException("Unknown user type: " + type);
             }
         } else {
+            assertFalse("User type cannot be null.",false);
+
             throw new IllegalArgumentException("User type cannot be null.");
         }
     }
+    
 
     @When("I submit the form")
     public void i_submit_the_form() {
@@ -81,18 +102,24 @@ public class Admin_add_aothers_Steps {
             } else {
                 msg = "All fields are required.";
             }
+            assertFalse("There is a loss",false);
             return;
         }
 
       
         if (app.userExists(username)) {
             msg = "Username already exists. Please choose a different username.";
+            assertTrue(app.userExists(username));
             return;
         }
 
       
         User newUser = new User(username, password, email, kindofuser);
+        newUser.setKindofuser(kindofuser);
+        newUser.setUsername(username);
+        newUser.toString();
         app.addUser(newUser);
+        newUser.toString();
 
         switch (kindofuser) {
             case beneficiaryUser:
@@ -110,17 +137,26 @@ public class Admin_add_aothers_Steps {
             default:
                 msg = "User type is not recognized.";
         }
+        assertTrue(app.isAdduser());
     }
+    
 
     @When("I leave the password field empty")
     public void i_leave_the_password_field_empty() {
         this.password = null; 
+        assertNull("Password is null", this.password);
+
+        
     }
+    
 
     @When("I do not select a user type")
     public void i_do_not_select_a_user_type() {
         this.kindofuser = null; 
+        assertNull("kindofuser is null", this.kindofuser);
+
     }
+    
 
     @Then("I should see {string}")
     public void i_should_see(String Message) {
@@ -128,6 +164,7 @@ public class Admin_add_aothers_Steps {
         assertEquals(Message, msg);
     }
 
+    
 
     @Given("the user {string} already exists")
     public void the_user_already_exists(String username) {
@@ -135,6 +172,7 @@ public class Admin_add_aothers_Steps {
         if (!app.userExists(username)) {
             User newUser = new User(username, "9999", username + "@gmail.com", Type.beneficiaryUser);
             app.addUser(newUser);
+            assertTrue("the user exist",!app.userExists(username));
         }
     }
 }
