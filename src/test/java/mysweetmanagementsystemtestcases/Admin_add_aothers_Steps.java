3a6d64f5e4ec24092f3361d65fc55a2469ff3000
Ruba Abu Samra <rubaabusam@gmail.com>
2024-08-22 23:25:18 +0300

    Update Admin_add_aothers_Steps.java

--- src/test/java/mysweetmanagementsystemtestcases/Admin_add_aothers_Steps.java
+++ src/test/java/mysweetmanagementsystemtestcases/Admin_add_aothers_Steps.java
@@ -3,6 +3,7 @@ package mysweetmanagementsystemtestcases;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 
 
 
